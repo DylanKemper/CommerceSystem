daf5bb0ac0b3acb5b6b4f250310cd177a29aa34c
DylanKemper <dylankemper7@gmail.com>
2026-08-26 12:43:39 +0200

    Created the OrderItem Model

---
+++ Stockify/Models/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Stockify.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }     // PK
+        public int OrderId { get; set; }     // FK to Order
+        public int ProductId { get; set; }     // FK to Product
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
