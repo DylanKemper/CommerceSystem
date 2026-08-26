f3863603fb62b95ca1129b5c3783ea96fedf5adb
DylanKemper <dylankemper7@gmail.com>
2026-08-26 17:05:10 +0200

    Added the OrderController and api controller.

---
+++ Stockify/Controllers/OrdersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stockify.Models;
+using Stockify.Services;
+
+namespace Stockify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> GetAll()
+        {
+            return Ok(_orderService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetById(int id)
+        {
+            var order = _orderService.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public ActionResult<Order> Create(Order order)
+        {
+            var createdOrder = _orderService.Add(order);
+            return CreatedAtAction(nameof(GetById), new { id = createdOrder.Id }, createdOrder);
+        }
+
+        [HttpPut]
+        public IActionResult Update(int id, Order order)
+        {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+            return _orderService.Update(order) ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return _orderService.Delete(id) ? NoContent() : NotFound();
+        }
+    }
+}
