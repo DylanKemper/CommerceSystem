0b8bb11d668c477febf512026d05996d74114877
DylanKemper <dylankemper7@gmail.com>
2026-08-26 12:41:53 +0200

    Created the Order Model

---
+++ Stockify/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Stockify.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }     // PK
+        public int CustomerId { get; set; }     // FK to Customer
+        public DateTime OrderDate { get; set; }
+        public List<OrderItem> Items { get; set; } = new();
+    }
+}
